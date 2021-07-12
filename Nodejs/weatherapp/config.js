4c97ffab5f1761868ff3d61079328e0a2bea79d5
A DHANARAJ <70677726+dhanaraj5432@users.noreply.github.com>
2021-07-12 16:42:30 +0530

    Add files via upload

---
+++ Nodejs/weatherapp/config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "6e4e07dd3f8d62d25ac6a7ffb15765f2"
+    }
+}
+
+module.exports = constants;
