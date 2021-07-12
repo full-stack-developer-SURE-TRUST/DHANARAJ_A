91cee61b49fc111d0a4b0b9aba3c9433db1f86c0
A DHANARAJ <70677726+dhanaraj5432@users.noreply.github.com>
2021-07-12 17:09:37 +0530

    Update node2.js

--- Nodejs/node2.js
+++ Nodejs/node2.js
@@ -2,15 +2,8 @@ console.log("Node2 file");
 var http = require("http");
 
 http.createServer(function (request, response) {  
-    // Send the HTTP header   
-    // HTTP Status: 200 : OK  
-    // Content Type: text/plain  
-    response.writeHead(200, {'Content-Type': 'text/plain'});  
-    // Send the response body as "Hello World"  
-    response.end('Hello World\n');  
+    response.writeHead(200, {'Content-Type': 'text/plain'});    
+    response.end('Hello\n');  
  }).listen(8081);  
  // Console will print the message  
- console.log('Server running at http://127.0.0.1:8081/');  
-
- // npm init -y
- // npm install express
+ console.log('Server running at http://127.0.0.1:8081/'); 
